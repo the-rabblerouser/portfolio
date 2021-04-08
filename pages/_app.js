3688a3eae354f53882ab11bda4568b0babda4623
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-04-07 23:20:04 -0500

    :root styling added to _app.js. Navbar component created

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import Head from 'next/head';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+	const [darkMode, setDarkMode] = useState();
 	return (
 		<>
 			<Head>
@@ -10,6 +12,17 @@ function MyApp({ Component, pageProps }) {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Component {...pageProps} />
+			<style jsx global>{`
+				:root {
+					--bg-color: ${!darkMode ? '#fff' : '#202020'};
+					--font-color: ${!darkMode ? '#202020' : '#fff'};
+					--accent-color: #82adc9;
+					--projects-title-color: ${!darkMode ? '#82adc9' : '#1c1c1c'};
+					--projects-color: ${!darkMode ? '#fff' : '#1c1c1c'};
+					--card-color: ${!darkMode ? '#fff' : '#1c1c1c'};
+					--card-description-color: ${!darkMode ? '#1c1c1c' : '#fff'};
+				}
+			`}</style>
 		</>
 	);
 }
