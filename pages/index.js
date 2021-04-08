43d72f5b76f48a3ac2564888ffa8f4045de6fa7c
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-04-07 23:59:34 -0500

    home page styles moved to Home.module.css

--- pages/index.js
+++ pages/index.js
@@ -5,67 +5,25 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
 	return (
 		<>
-			<div className="home-page-hero">
+			<div>
 				<div className={styles.container}>
-					<h1 className="hero-text ">Hi, I'm Joe</h1>
-					<div className="sliding-text">
-						<p>
-							and i'm your next{' '}
-							<span>
-								<ReactRotatingText
-									typingInterval={140}
-									deletingInterval={140}
-									pause={2000}
-									color="#82ADC9"
-									items={['react', 'next', 'node']}
-								/>
-							</span>
-							.js developer
-						</p>
-					</div>
-					<div className="links">{/* <LinkIcons /> */}</div>
+					<h1 className={styles.home}>Hi, I'm Joe</h1>
+					<p className={styles.slidingText}>
+						and i'm your next{' '}
+						<span>
+							<ReactRotatingText
+								typingInterval={140}
+								deletingInterval={140}
+								pause={2000}
+								color="#82ADC9"
+								items={['react', 'next', 'node']}
+							/>
+						</span>
+						.js developer
+					</p>
+					<div className={styles.links}>{/* <LinkIcons /> */}</div>
 				</div>
 			</div>
-
-			<style jsx>
-				{`
-					.hero-text {
-						font-size: 80px;
-						font-weight: 600;
-						margin: 0;
-						color: var(--font-color);
-					}
-					.sliding-text p {
-						font-size: 33px;
-						font-family: 'Inconsolata', monospace;
-						font-weight: 200;
-						color: var(--font-color);
-					}
-					.react-rotating-text-cursor {
-						animation: blinking-cursor 0.8s cubic-bezier(0.68, 0.01, 0.01, 0.99)
-							0s infinite;
-					}
-					.links {
-						display: flex;
-						justify-content: flex-end;
-						margin-top: 2em;
-						margin-right: 2em;
-					}
-					//  ***** Media Queries *****
-
-					@media (max-width: 768px) {
-						.hero-text {
-							font-size: 60px;
-						}
-						.sliding-text p {
-							font-size: 15px;
-							font-weight: 300;
-						}
-					}
-					@media (max-width: 812px) {
-					}
-				`}
-			</style>
 		</>
 	);
 }
