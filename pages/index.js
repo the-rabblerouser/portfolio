d2e61a1233abcf153eeeebddf0b220b0c85b823e
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-04-11 14:42:50 -0500

    moved Arrow svg to Icons.js and added Arrow component to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import ReactRotatingText from 'react-rotating-text';
-import LinkIcons from '../components/LinkIcons';
 
+import { Arrow } from '../components/Icons';
+import LinkIcons from '../components/LinkIcons';
 import styles from '../styles/Home.module.css';
 
 import {
@@ -37,18 +38,7 @@ export default function Home() {
 					<div>
 						<a href="#about">
 							<button className="btn mt-4">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									fill="currentColor"
-									className="bi bi-arrow-down"
-									viewBox="0 0 16 16">
-									<path
-										fillRule="evenodd"
-										d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-									/>
-								</svg>
+								<Arrow />
 							</button>
 						</a>
 					</div>
