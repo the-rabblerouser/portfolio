6f712b610d3c25982e9cfd1334ed4a8123862f11
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-04-11 07:51:00 -0500

    react-spring added. Projects page and styles sheet intial setup

---
+++ pages/projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from '../styles/Projects.module.css';
+
+const projects = () => {
+	return (
+		<>
+			<div className={styles.container}>Hello World</div>
+		</>
+	);
+};
+
+export default projects;
