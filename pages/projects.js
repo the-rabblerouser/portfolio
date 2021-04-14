8eb81f7fade500040e3634288f87a9e42889d164
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-04-14 14:45:56 -0500

    ProjectCard was not necessary. code for card moved to project.js

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,6 @@
 import { useSpring, animated, config } from 'react-spring';
 import useSwr from 'swr';
 
-import ProjectCard from '../components/ProjectCard';
 import Footer from '../components/Footer';
 
 import styles from '../styles/Projects.module.css';
@@ -26,7 +25,30 @@ const projects = () => {
 				<animated.h1 className={styles.projectTitle} style={titleAnimation}>
 					projects
 				</animated.h1>
-				<ProjectCard data={data} />
+				{data.map(({ _id, title, repository, description, tools, site }) => {
+					return (
+						<div key={_id} className={styles.projectContainer}>
+							<div>
+								<div className={styles.projectHeader}>
+									<a className={styles.links} href={site}>
+										<h4>{title}</h4>
+									</a>
+									<div className={styles.code}>
+										<span>-</span>
+										<a className={styles.links} href={repository}>
+											{' '}
+											code
+										</a>
+									</div>
+								</div>
+								<div>
+									<div className={styles.tools}>{tools}</div>
+									<div>{description}</div>
+								</div>
+							</div>
+						</div>
+					);
+				})}
 			</div>
 			<Footer />
 		</>
