b03b8858f13a915aa11883b1fa68e3f2854e86f1
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-04-13 21:50:54 -0500

    react-scroll added to index.js for smooth scroll

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
-import Link from 'next/link';
 import ReactRotatingText from 'react-rotating-text';
 import { useSpring, animated, config } from 'react-spring';
+import { Link } from 'react-scroll';
 
 import { Arrow } from '../components/Icons';
-
-import styles from '../styles/Home.module.css';
-
 import {
 	Node,
 	React,
 	Typescript,
 	Javascript,
 	Mongodb,
-	Next,
 } from '../components/Icons';
+
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
 	const homeAnimation = useSpring({
@@ -35,88 +33,91 @@ export default function Home() {
 
 	return (
 		<>
-			<div data-bs-spy="scroll" data-bs-target="#about">
-				<animated.div style={homeAnimation} className={styles.container}>
-					<h1 className={styles.home}>Hi, I'm Joe...</h1>
-					<p className={styles.slidingText}>
-						your new{' '}
-						<span>
-							<ReactRotatingText
-								typingInterval={140}
-								deletingInterval={140}
-								pause={2000}
-								color="#82ADC9"
-								items={['frontend', 'backend', 'fullstack']}
-							/>
-						</span>
-						developer
-					</p>
-					<div style={{ color: 'var(--font-color)' }}>check me out</div>
-					<div className={styles.arrowButton}>
-						<a href="#about">
-							<animated.div style={arrowAnimation}>
-								<Arrow />
-							</animated.div>
-						</a>
+			<animated.div style={homeAnimation} className={styles.container}>
+				<h1 className={styles.home}>Hi, I'm Joe...</h1>
+				<p className={styles.slidingText}>
+					your new{' '}
+					<span>
+						<ReactRotatingText
+							typingInterval={140}
+							deletingInterval={140}
+							pause={2000}
+							color="#82ADC9"
+							items={['frontend', 'backend', 'fullstack']}
+						/>
+					</span>
+					developer
+				</p>
+				<div style={{ color: 'var(--font-color)' }}>check me out</div>
+				<div className={styles.arrowButton}>
+					<animated.div style={arrowAnimation}>
+						<Link
+							activeClass="active"
+							to="about"
+							spy={true}
+							smooth={true}
+							duration={1000}>
+							<Arrow />
+						</Link>
+					</animated.div>
+				</div>
+			</animated.div>
+			<div className={styles.aboutMe}>
+				<h1 className={styles.aboutTitle} id="about">
+					about me
+				</h1>
+				<div className={styles.aboutDesc}>
+					<div>
+						<img
+							className={styles.img}
+							src="./F6835DBC-1D6F-45E3-A9DA-14B29B32CDB9_1_105_c.jpeg"
+							alt="Joe"
+						/>
 					</div>
-				</animated.div>
-				<div className={styles.aboutMe}>
-					<h1 className={styles.aboutTitle} id="about">
-						about me
-					</h1>
-					<div className={styles.aboutDesc}>
-						<div>
-							<img
-								className={styles.img}
-								src="./F6835DBC-1D6F-45E3-A9DA-14B29B32CDB9_1_105_c.jpeg"
-								alt="Joe"
-							/>
-						</div>
-						<div>
-							<p style={{ marginBottom: '1.2rem' }}>
-								Hi! My name is{' '}
-								<span className={styles.contactName}>Joe McCann</span>. I am a
-								junior Full Stack Web Developer who specializes in React, Node
-								and Next.js. I love spending time building projects in
-								javascript and learning about new technologies. I am currently
-								looking for work. Send me a message, I would love to work with
-								you in the near future!
-							</p>
+					<div>
+						<p style={{ marginBottom: '1.2rem' }}>
+							Hi! My name is{' '}
+							<span className={styles.contactName}>Joe McCann</span>. I am a
+							junior Full Stack Web Developer who specializes in React, Node and
+							Next.js. I love spending time building projects in javascript and
+							learning about new technologies. I am currently looking for work.
+							Send me a message, I would love to work with you in the near
+							future!
+						</p>
 
-							<div className={styles.skillSection}>
-								<h4 className={styles.skillsTitle}>look at what i can do</h4>
-								<div className={styles.skills}>
-									<div>
-										<h5 className={styles.skillsSubTitle}>languages</h5>
-										<ul className={styles.skillsList}>
-											<li>
-												<Javascript /> javascript
-											</li>
-											<li>
-												<Node /> node.js
-											</li>
-											<li>
-												<Typescript /> typescript
-											</li>
-										</ul>
-									</div>
-									<div>
-										<h5 className={styles.skillsSubTitle}>technologies</h5>
-										<ul className={styles.skillsList}>
-											<li>
-												<React /> react.js
-											</li>
-											<li>
-												<Javascript /> next.js
-											</li>
-											<li>
-												<Javascript /> express
-											</li>
-											<li>
-												<Mongodb /> mongodb
-											</li>
-										</ul>
-									</div>
+						<div className={styles.skillSection}>
+							<h4 className={styles.skillsTitle}>look at what i can do</h4>
+							<div className={styles.skills}>
+								<div>
+									<h5 className={styles.skillsSubTitle}>languages</h5>
+									<ul className={styles.skillsList}>
+										<li>
+											<Javascript /> javascript
+										</li>
+										<li>
+											<Node /> node.js
+										</li>
+										<li>
+											<Typescript /> typescript
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h5 className={styles.skillsSubTitle}>technologies</h5>
+									<ul className={styles.skillsList}>
+										<li>
+											<React /> react.js
+										</li>
+										<li>
+											<Javascript /> next.js
+										</li>
+										<li>
+											<Javascript /> express
+										</li>
+										<li>
+											<Mongodb /> mongodb
+										</li>
+									</ul>
 								</div>
 							</div>
 						</div>
